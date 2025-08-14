10e43909d5243ee2fd0b00114119b8a2adf26efc
kabarab <nazarzukov0@gmail.com>
2025-08-14 22:11:01 +0300

    Fix component file names

--- src/components/PaneContextMenu.jsx
+++ src/components/PaneContextMenu.jsx
@@ -1,14 +1,9 @@
-// ИСПРАВЛЕНИЕ: Используем переименованный файл стилей
-import './NodeContextMenu.css'; 
+// Было: import './ContextMenu.css';
+// Стало:
+import './NodeContextMenu.css';
 
 function PaneContextMenu({ top, left, onAction }) {
-  return (
-    <div className="context-menu" style={{ top, left }}>
-      <button onClick={() => onAction('addTelegramNode')}>Добавить узел Telegram</button>
-      <button onClick={() => onAction('addDefaultNode')}>Добавить стандартный узел</button>
-      {/* Сюда можно будет добавить другие узлы */}
-    </div>
-  );
+  // ... остальной код без изменений
 }
 
 export default PaneContextMenu;
